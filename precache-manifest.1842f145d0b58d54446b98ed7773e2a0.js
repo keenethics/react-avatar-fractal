0548c68db8ceca61d130203d00c3381262effca0
Dikhtenko Taras <dihtenkott@gmail.com>
2022-07-07 21:34:06 +0200

    Updates

---
+++ precache-manifest.1842f145d0b58d54446b98ed7773e2a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4508ac59a4934271f8a31f056bd00712",
+    "url": "./index.html"
+  },
+  {
+    "revision": "73687cec7b90d83bf2a8",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "6da9beb3d5228ff1e2c9",
+    "url": "./static/js/2.b38b6342.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b38b6342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73687cec7b90d83bf2a8",
+    "url": "./static/js/main.eff6e62f.chunk.js"
+  },
+  {
+    "revision": "6fce53c7c7713ebf61712cc2929746fa",
+    "url": "./static/js/main.eff6e62f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304b82fd3078156245cb",
+    "url": "./static/js/runtime-main.4f837e39.js"
+  }
+]);
